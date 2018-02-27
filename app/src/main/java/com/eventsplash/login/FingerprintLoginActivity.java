0277b9d6b96f7fd641bf1d44a34e6f1ffb19bf04
Jonathan Havstad <jonathanhavstad@Jonathans-MBP.home>
2018-02-27 08:16:46 -0500

    Added Jacoco reports and intial setup for the Fingerprint reader login.

---
+++ app/src/main/java/com/eventsplash/login/FingerprintLoginActivity.java
@@ -0,0 +1,11 @@
+package com.eventsplash.login;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jonathanhavstad on 2/26/18.
+ */
+
+public class FingerprintLoginActivity extends AppCompatActivity {
+
+}
