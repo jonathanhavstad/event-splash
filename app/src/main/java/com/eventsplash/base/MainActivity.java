9ac7d85b1e8517245c7f56304ff62b0d765067fa
Jonathan Havstad <jonathanhavstad@Jonathans-MBP.home>
2018-03-01 10:20:53 -0500

    Removed the Menu option from the Home screen.

--- app/src/main/java/com/eventsplash/base/MainActivity.java
+++ app/src/main/java/com/eventsplash/base/MainActivity.java
@@ -65,37 +65,7 @@ public class MainActivity extends AppCompatActivity implements
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
 
-        if (Build.VERSION_CODES.M <= Build.VERSION.SDK_INT) {
-            checkForLocationPermission();
-        } else {
-            fineLocationPermissionGranted = true;
-            loadHomeFragments();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_login) {
-            return true;
-        } else if (id == android.R.id.home) {
-            drawerLayout.openDrawer(GravityCompat.START);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        checkForLocationPermission();
     }
 
     @Override
